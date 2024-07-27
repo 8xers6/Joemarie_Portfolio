0d267c584289f501e2eb780e1bb7e6a3086fe389
8xers6 <109412652+8xers6@users.noreply.github.com>
2024-07-27 15:23:13 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,17 +4,20 @@ import Aboutme from './Aboutme';
 import ListofProgramming from './ListofProgramming';
 import Somethingaboutme from './Somethingaboutme';
 import ContactUsSection from './Contactme';
+import Guessinggames from './Guessinggames';
 import Footer from './Footer';
 function App() {
   return (
 
       
       <>
+      <title>Joemarie Jacinto</title>
       <Navbar />
       <Aboutme/>
       <Somethingaboutme/>
       <ListofProgramming/>
       <ContactUsSection/>
+      <Guessinggames/>
       <Footer/>
 
     
