ffde91631f97d059f6ee244b642904b2fe2d1f83
8xers6 <109412652+8xers6@users.noreply.github.com>
2024-07-27 15:22:51 +0800

    Update Aboutme.jsx

--- src/Aboutme.jsx
+++ src/Aboutme.jsx
@@ -1,31 +1,53 @@
-import sampleImage from './Assets/fbfb.jpeg';
+import React from 'react';
+import sampleImage from './Assets/fbfb.jpeg'; 
+import 'animate.css';
 
-function Aboutme(){
+function AboutMe() {
+  const handleScroll = (targetId) => {
+    const targetElement = document.getElementById(targetId);
+    if (targetElement) {
+      targetElement.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+      });
+    }
+  };
 
-    return(    
-        
-        <div id="Aboutme"className="flex items-center justify-center h-screen bg-gray-200">
-        <div className="max-w-4xl bg-white rounded-lg shadow-lg overflow-hidden">
-            <img src={sampleImage} alt="Sample" className="w-full h-auto" />
-            <div className="p-6">
-                <h2 className="text-2xl font-bold text-gray-800 flex justify-center" >Hello Im&nbsp;<b>Joemarie Jacinto</b></h2>
-                <p className="mt-2 text-gray-600" >I Just Recently Graduated From Pambayang Dalubhasaan ng Marilao.</p>
-                <p className="mt-2 text-gray-600 flex justify-center" >Im Looking for a Work to Show case my Skills and Talent</p>
-            </div>
+  return (
+    <div id="Aboutme" className="flex flex-col items-center justify-center min-h-screen bg-gray-200 pt-3 px-3  pb-12">
+      <div className="text-center p-4">
+        <div className="pb-12">
+            <span className="text-yellow-500 text-5xl ">Development to </span>
+            <span className="text-green-600 text-6xl "> Growth to </span>
+            <span className="text-red-500 text-7xl "><b>Success</b></span>
         </div>
+        <img
+          src={sampleImage}
+          alt="Joemarie Jacinto"
+          className="w-32 h-32 md:w-48 md:h-48 rounded-full object-cover mx-auto"
+        />
+        <div className="mt-4">
+          <h2 className="text-2xl font-bold text-gray-800">Hello, I'm <b>Joemarie Jacinto</b></h2>
+          <p className="mt-2 text-gray-600">I just recently graduated from Pambayang Dalubhasaan ng Marilao.</p>
+          <p className="mt-2 text-gray-600">I'm looking for a job to showcase my skills and talent.</p>
+        </div>
+      </div>
+      <div className="p-4 flex flex-col sm:flex-row justify-center gap-2">
+        <button
+          onClick={() => handleScroll('Contactme')}
+          className="bg-purple-400 hover:bg-purple-500 text-white font-bold py-2 px-4 rounded"
+        >
+          Contact Me
+        </button>
+        <button
+          onClick={() => handleScroll('DownloadCV')}
+          className="bg-purple-400 hover:bg-purple-500 text-white font-bold py-2 px-4 rounded "
+        >
+          Download My CV
+        </button>
+      </div>
     </div>
-
-
-
-
-
-);
-
-
-
-
-
-
+  );
 }
 
-export default Aboutme
+export default AboutMe;
