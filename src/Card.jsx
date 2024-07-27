d58ed86c190fd7b8cb315482c22eadc6306dc4ff
8xers6 <109412652+8xers6@users.noreply.github.com>
2024-07-27 15:23:27 +0800

    Update Card.jsx

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,6 +1,6 @@
 const Card = ({ image, alt }) => {
     return (
-        <div id="listofknown" className="max-w-xs mx-auto mb-4 relative overflow-hidden transform transition duration-300 hover:scale-105">
+        <div  className="max-w-xs mx-auto mb-4 relative overflow-hidden transform transition duration-300 hover:scale-105">
             <div className="flex items-center justify-center w-full h-48 rounded-lg overflow-hidden shadow-lg bg-white">
                 <img className="w-full h-full object-cover" src={image} alt={alt} />
             </div>
